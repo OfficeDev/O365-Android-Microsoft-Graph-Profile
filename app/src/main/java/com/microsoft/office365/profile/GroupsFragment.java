4ef7a9f1a14dad194a934a7f6ad88536ac191db2
Ricardo Loo <ricardol@microsoft.com>
2015-04-16 15:34:16 -0700

    Changed files and classes names from 'Membership' to 'Group'

---
+++ app/src/main/java/com/microsoft/office365/profile/GroupsFragment.java
@@ -0,0 +1,35 @@
+package com.microsoft.office365.profile;
+
+/**
+ * A fragment representing a list of Items.
+ * <p/>
+ * <p/>
+ */
+public class GroupsFragment extends BaseUserListFragment {
+    protected static final String TAG = "GroupsFragment";
+
+    /**
+     * Mandatory empty constructor for the fragment manager to instantiate the
+     * fragment (e.g. upon screen orientation changes).
+     */
+    public GroupsFragment() {
+    }
+
+    public String getEndpoint(){
+        return "/users/" + ((ProfileActivity)getActivity()).getUserId() + "/memberof";
+    }
+
+    @Override
+    public int getTitleResourceId() {
+        return R.string.fragment_groups_title;
+    }
+
+    /**
+     * Returns the message to display when there are no direct reports returned by a request.
+     * @return The message to display if there are no direct reports.
+     */
+    @Override
+    public CharSequence getEmptyArrayMessage() {
+        return getResources().getText(R.string.empty_array_groups_fragment_message);
+    }
+}
--- app/src/main/java/com/microsoft/office365/profile/model/Group.java
+++ app/src/main/java/com/microsoft/office365/profile/model/Group.java
@@ -3,5 +3,7 @@ package com.microsoft.office365.profile.model;
 /**
  * Created by Administrator on 4/6/2015.
  */
-public class Groups {
+public class Group {
+    public String objectId;
+    public String displayName;
 }
