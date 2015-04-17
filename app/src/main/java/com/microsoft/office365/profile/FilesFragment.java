d1d47230bb3c35bb3800c17fae0c2815b790ff50
Ricardo Loo <ricardol@microsoft.com>
2015-04-16 19:22:53 -0700

    First version of working files fragment

--- app/src/main/java/com/microsoft/office365/profile/FilesFragment.java
+++ app/src/main/java/com/microsoft/office365/profile/FilesFragment.java
@@ -1,35 +1,28 @@
 package com.microsoft.office365.profile;
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AbsListView;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.ListAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.reflect.TypeToken;
+import com.microsoft.office365.profile.model.File;
 
-import com.microsoft.office365.profile.dummy.DummyContent;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 
 /**
  * A fragment representing a list of Items.
+ * <p/>
+ * <p/>
  */
-public class FilesFragment extends Fragment implements AbsListView.OnItemClickListener {
-
-    /**
-     * The fragment's ListView/GridView.
-     */
-    private AbsListView mListView;
-
-    /**
-     * The Adapter which will be used to populate the ListView/GridView with
-     * Views.
-     */
-    private ListAdapter mAdapter;
+public class FilesFragment extends BaseListFragment {
+    protected static final String TAG = "FilesFragment";
+    ArrayList<File> mFileList;
 
     /**
      * Mandatory empty constructor for the fragment manager to instantiate the
@@ -38,46 +31,64 @@ public class FilesFragment extends Fragment implements AbsListView.OnItemClickLi
     public FilesFragment() {
     }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-
-        // TODO: Change Adapter to display your content
-        mAdapter = new ArrayAdapter<DummyContent.DummyItem>(getActivity(),
-                android.R.layout.simple_list_item_1, android.R.id.text1, DummyContent.ITEMS);
+    public String getEndpoint(){
+        return "/users/" + ((ProfileActivity)getActivity()).getUserId() + "/files";
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_files, container, false);
-
-        // Set the adapter
-        mListView = (AbsListView) view.findViewById(android.R.id.list);
-        ((AdapterView<ListAdapter>) mListView).setAdapter(mAdapter);
-
-        // Set OnItemClickListener so we can be notified on item clicks
-        mListView.setOnItemClickListener(this);
-
-        return view;
-    }
-
-    @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        //TODO: Call the ProfileActivity Intent with the id of the current user
+    public int getTitleResourceId() {
+        return R.string.fragment_files_title;
     }
 
     /**
-     * The default content for this Fragment has a TextView that is shown when
-     * the list is empty. If you would like to change the text, call this method
-     * to supply the text it should use.
+     * Returns the message to display when there are no direct reports returned by a request.
+     * @return The message to display if there are no direct reports.
      */
-    public void setEmptyText(CharSequence emptyText) {
-        View emptyView = mListView.getEmptyView();
+    @Override
+    public CharSequence getEmptyArrayMessage() {
+        return getResources().getText(R.string.empty_array_files_fragment_message);
+    }
 
-        if (emptyView instanceof TextView) {
-            ((TextView) emptyView).setText(emptyText);
+    @Override
+    public void onRequestSuccess(final JsonElement data) {
+        Gson gson = new Gson();
+
+        Type listType = new TypeToken<ArrayList<File>>() { }.getType();
+
+        if(((JsonObject) data).has("value")) {
+            mFileList = gson.fromJson(((JsonObject) data).getAsJsonArray("value"), listType);
+        } else {
+            mFileList = new ArrayList<>();
+            File justOneFile = gson.fromJson(data, File.class);
+            mFileList.add(justOneFile);
         }
+
+        getActivity().runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                LayoutInflater layoutInflater = (LayoutInflater) getActivity().getSystemService(getActivity().LAYOUT_INFLATER_SERVICE);
+                View header = layoutInflater.inflate(R.layout.header_base_list, null);
+                TextView title = (TextView) header.findViewById(R.id.title);
+                title.setText(getTitleResourceId());
+
+                ListView listView = getListView();
+                listView.addHeaderView(header);
+
+                // I don't want to accept any clicks
+                listView.setEnabled(false);
+
+                // If there are no elements, display a custom message
+                if (mFileList.size() == 0) {
+                    File noData = new File();
+                    noData.name = (String)getEmptyArrayMessage();
+                    mFileList.add(noData);
+                }
+                setListAdapter(new ArrayAdapter<>(
+                        getActivity(),
+                        android.R.layout.simple_list_item_1,
+                        mFileList));
+                setListShown(true);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/microsoft/office365/profile/model/File.java
@@ -0,0 +1,32 @@
+package com.microsoft.office365.profile.model;
+
+import android.support.annotation.NonNull;
+
+/**
+ * Created by ricardol on 4/16/2015.
+ */
+public class File implements CharSequence {
+    public String id;
+    public String name;
+    public BasicUserInfo lastModifiedBy;
+
+    @Override
+    public int length() {
+        return name.length();
+    }
+
+    @Override
+    public char charAt(int index) {
+        return name.charAt(index);
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    @Override
+    public CharSequence subSequence(int start, int end) {
+        return name.subSequence(start, end);
+    }
+}
