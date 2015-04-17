6b6c18d46157fc5c29c3bc3e35e34850e3315106
Ricardo Loo <ricardol@microsoft.com>
2015-04-16 17:27:09 -0700

    Added onRequestFailure method to ManagerFragment

--- app/src/main/java/com/microsoft/office365/profile/ManagerFragment.java
+++ app/src/main/java/com/microsoft/office365/profile/ManagerFragment.java
@@ -1,5 +1,15 @@
 package com.microsoft.office365.profile;
 
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+
 /**
  * A fragment representing a list of Items.
  * <p/>
@@ -31,5 +41,39 @@ public class ManagerFragment extends UserListFragment {
     @Override
     public CharSequence getFileNotFoundExceptionMessage() {
         return getResources().getText(R.string.file_not_found_exception_manager_fragment_message);
+    }
+
+    @Override
+    public void onRequestFailure(Exception e) {
+        Log.e(TAG, e.getMessage());
+
+        //If we have a FileNotFoundException it's because this user doesn't have a manager
+        if(e.getClass().equals(FileNotFoundException.class)) {
+            getActivity().runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    LayoutInflater layoutInflater = (LayoutInflater) getActivity().getSystemService(getActivity().LAYOUT_INFLATER_SERVICE);
+                    View header = layoutInflater.inflate(R.layout.header_base_list, null);
+                    TextView title = (TextView) header.findViewById(R.id.title);
+                    title.setText(getTitleResourceId());
+
+                    ArrayList<String> notAvailableList = new ArrayList<>();
+                    notAvailableList.add("He doesn't report to anybody");
+
+                    ListView listView = getListView();
+                    listView.addHeaderView(header);
+                    listView.setClickable(false);
+                    listView.setEnabled(false);
+
+                    setListAdapter(new ArrayAdapter<>(
+                            getActivity(),
+                            android.R.layout.simple_list_item_1,
+                            notAvailableList));
+                    setListShown(true);
+                }
+            });
+        } else { // If it's not a FileNotFoundException then let the parent handle the error
+            super.onRequestFailure(e);
+        }
     }
 }
